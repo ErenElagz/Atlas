51c78d6e099dbd97ce8273e4a90a96083c81bf8f
ErenElagz <erenelagoz@gmail.com>
2024-04-08 21:16:48 +0300

    Maps

---
+++ Atlas Maps/marker.js
@@ -0,0 +1,35 @@
+import React from "react";
+import IstanbulData from "./places.json";
+import { Marker } from "react-native-maps";
+import { StyleSheet } from "react-native";
+
+export default function MarkerComponentCallout({ setData }) {
+    const sendData = () => {
+        const data = [];
+        setData(data);
+    };
+
+    const destinations = IstanbulData.destination.best_destinations;
+
+    return (
+        <>
+            {destinations.map((destination, index) => (
+                <Marker
+                    style={styles.marker}
+                    key={index}
+                    coordinate={destination}
+                    title={destination.name}
+                    onPress={() => {
+                        setData(destination);
+                        sendData;
+                    }}
+                ></Marker>
+            ))}
+        </>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
