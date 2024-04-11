d684b87592a07010542d8827cd40d537823ff1be
ErenElagz <erenelagoz@gmail.com>
2024-04-11 10:25:00 +0300

    ForgotPassword minifieded and formated code

--- src/screens/Login/ForgotPassword.js
+++ src/screens/Login/ForgotPassword.js
@@ -1,145 +1,48 @@
 import React from "react";
-import { View, StyleSheet, Text, Dimensions, Pressable, TextInput } from "react-native";
+import { View, StyleSheet, Text, Pressable, TextInput } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function ForgotPassword({ navigation }) {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-            <View>
-                <View style={styles.header}>
-                    <Text style={styles.title}>Forgot Password</Text>
-                </View>
-            </View>
-
-            <View>
-                <View style={{ paddingHorizontal: 32, marginTop: 32, gap: 8 }}>
-                    <TextInput placeholder="E-mail" style={styles.input} selectionColor={"#323232"}></TextInput>
-                </View>
-                <View style={styles.sign}>
-                    <Pressable style={styles.sign2}>
-                        <Text style={styles.text2}>Send Email</Text>
-                    </Pressable>
-                </View>
-                <Pressable onPress={() => navigation.navigate("SignIn")}>
-                    <Text
-                        style={{
-                            fontSize: 14,
-                            fontWeight: "600",
-                            textAlign: "center",
-                            marginTop: 8,
-                            color: "#969696",
-                            textDecorationLine: "underline"
-                        }}
-                    >
-                        Go Back
-                    </Text>
-                </Pressable>
-            </View>
+            <Text style={styles.title}>Forgot Password</Text>
+            <TextInput
+                placeholder="E-mail"
+                style={styles.input}></TextInput>
+            <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>Send Email</Text>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate("SignIn")}>
+                <Text style={styles.goBack}>Go Back</Text>
+            </Pressable>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 32,
+        paddingTop: 64,
+        padding: 20,
         alignItems: "center",
         backgroundColor: "#fff",
-        gap: 8
-    },
-    header: {
-        flexDirection: "row",
-        alignItems: "center",
-        paddingHorizontal: 16,
-        marginTop: 32,
-        gap: 4
+        gap: 16
     },
     title: {
         fontSize: 32,
-        fontWeight: "700"
+        fontWeight: "800",
+        color: "#323232"
     },
-    logo: {
-        width: 32,
-        height: 32
-    },
-    logo2: {
-        width: 24,
-        height: 24
-    },
-    slogan: {
-        fontSize: 14,
-        color: "#aaa"
-    },
-    img: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").width,
-        aspectRatio: "1/1"
-    },
-    button: {},
-    sign: {
-        flexDirection: "row",
-        padding: 20,
-        width: "100%",
-        gap: 8,
-        alignSelf: "center"
-    },
-    sign2: {
+    button: {
         backgroundColor: "#323232",
         alignItems: "center",
         justifyContent: "center",
         padding: 16,
         borderRadius: 32,
-        flexGrow: 1
+        width: "100%"
     },
-    text1: {
-        fontSize: 14,
-        width: "100%",
-        textAlign: "center"
-    },
-    text2: {
-        fontSize: 14,
-        color: "#fff",
-        width: "100%",
-        textAlign: "center"
-    },
-    orView: {
-        flexDirection: "row",
-        paddingHorizontal: 48,
-        alignItems: "center",
-        gap: 16,
-        paddingVertical: 4
-    },
-    or: { fontSize: 12, color: "#bbb" },
-    div: { flexGrow: 1, backgroundColor: "#eee", height: 1.5 },
-    google: {
-        backgroundColor: "#fafafa",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 16,
-        borderRadius: 32,
-        width: "100%",
-        flexDirection: "row",
-        paddingHorizontal: 32,
-        gap: 16,
-        textAlign: "center"
-    },
-    facebook: {
-        backgroundColor: "#1877F2",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 16,
-        borderRadius: 32,
-        width: "100%",
-        flexDirection: "row",
-        gap: 16,
-        textAlign: "center",
-        paddingHorizontal: 32
-    },
-    thirdView: {
-        paddingHorizontal: 24,
-        alignItems: "center",
-        gap: 8,
-        paddingTop: 12
+    buttonText: {
+        color: "#fff"
     },
     input: {
         backgroundColor: "#fafafa",
@@ -148,5 +51,12 @@ const styles = StyleSheet.create({
         paddingVertical: 14,
         paddingHorizontal: 20,
         borderRadius: 12
+    },
+    goBack: {
+        fontSize: 14,
+        fontWeight: "600",
+        textAlign: "center",
+        color: "#969696",
+        textDecorationLine: "underline"
     }
 });
