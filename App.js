581a009d664e3c55a71b342b3e121ece92df11cd
ErenElagz <erenelagoz@gmail.com>
2024-05-05 12:00:57 +0300

    Pages Created

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 //Packages
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, StyleSheet, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -15,39 +15,59 @@ import SignUp from "./src/screens/Login/SignUp";
 import ForgotPassword from "./src/screens/Login/ForgotPassword";
 import HomeScreen from "./src/screens/HomeScreen";
 import CreateRoute from "./src/screens/CreateRoute";
+import SearchScreen from "./src/screens/SearchScreen";
+import CategoriesScreen from "./src/screens/CategoriesScreen";
+import PlacesScreen from "./src/screens/PlacesScreen";
+import RoutesScreen from "./src/screens/RoutesScreen";
 
 function Home({ navigation }) {
     return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            <Pressable style={styles.button} onPress={() => navigation.navigate("HomeScreen")}>
-                <Text>Home Screen</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("Map")}>
-                <Text>Maps</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("ChatBotScreen")}>
-                <Text>ChatBot</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("CreateRoute")}>
-                <Text>CreateRoute</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("ProfileScreen")}>
-                <Text>Profile</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("SplashScreen")}>
-                <Text>Splash</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("SignIn")}>
-                <Text>Sign In</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("SignUp")}>
-                <Text>Sign Up</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => navigation.navigate("ForgotPassword")}>
-                <Text>Forgot Password</Text>
-            </Pressable>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <Text>Login Page</Text>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("SplashScreen")}>
+                    <Text>Splash</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("SignIn")}>
+                    <Text>Sign In</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("SignUp")}>
+                    <Text>Sign Up</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("ForgotPassword")}>
+                    <Text>Forgot Password</Text>
+                </Pressable>
+                <Text>Screens</Text>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("HomeScreen")}>
+                    <Text>Home Screen</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("SearchScreen")}>
+                    <Text>Search Screen</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("CategoriesScreen")}>
+                    <Text>Categories Screen</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("PlacesScreen")}>
+                    <Text>Places Screen</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("RoutesScreen")}>
+                    <Text>Routes Screen</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("Map")}>
+                    <Text>Maps</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("ChatBotScreen")}>
+                    <Text>ChatBot</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("CreateRoute")}>
+                    <Text>CreateRoute</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("ProfileScreen")}>
+                    <Text>Profile</Text>
+                </Pressable>
+            </View>
+        </ScrollView>
     );
 }
 const Stack = createNativeStackNavigator();
@@ -71,6 +91,10 @@ export default function App() {
                 <Stack.Screen name="SignUp" component={SignUp} />
                 <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
                 <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="SearchScreen" component={SearchScreen} />
+                <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
+                <Stack.Screen name="PlacesScreen" component={PlacesScreen} />
+                <Stack.Screen name="RoutesScreen" component={RoutesScreen} />
                 <Stack.Screen name="CreateRoute" component={CreateRoute} />
             </Stack.Navigator>
         </NavigationContainer>
@@ -79,10 +103,11 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
+        alignItems: "left",
         justifyContent: "center",
-        gap: 20,
-        margin: 16
+        gap: 16,
+        margin: 16,
+        paddingTop: 16
     },
     button: {
         borderRadius: 8,
